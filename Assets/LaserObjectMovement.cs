79ccea520363218df655381d8d143d8e36fc62fa
Simsmeister <rr67wh@student.aau.dk>
2023-11-27 11:11:10 +0100

    Fiks til laser og skjold størrelse

--- Assets/LaserObjectMovement.cs
+++ Assets/LaserObjectMovement.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class LaserMovement : MonoBehaviour
+public class LaserObjectMovement : MonoBehaviour
 {
     public GameObject target; // The object we want to follow
     public float followDistance = 15f; // The distance behind the target object we want to follow
@@ -17,7 +17,7 @@ public class LaserMovement : MonoBehaviour
         // Store the original position of the object holding this script
         originalPosition = transform.position;
         target = GameObject.Find("Player");
-        transform.Rotate(90, 0, 0, Space.Self);
+        transform.Rotate(0, 0, 0, Space.Self);
         Destroy(gameObject, 15f); // Destroy the object after 10 second
     }
 
@@ -38,13 +38,13 @@ public class LaserMovement : MonoBehaviour
                 transform.position = new Vector3(originalPosition.x, originalPosition.y, targetPosition.z - followDistance);
             }
             // Otherwise, accelerate quickly and move past the target object
-            /*else
+            else
             {
                 float distance = acceleration * Time.deltaTime * Time.deltaTime;
                 
                 // Move our position ahead of the target object along the z-axis
                 transform.position += new Vector3(0f, 0f, distance);
-            }*/
+            }
 
             // Increment the elapsed time
             elapsedSeconds += Time.deltaTime;
